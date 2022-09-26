faacb50d8df3d28ffbb15f995808f9f83a772a28
milesi <irina-rxv@yandex.ru>
2022-09-26 22:55:55 +0300

    дз2 сделано

---
+++ main.py
@@ -0,0 +1,42 @@
+# a = input()
+# sum = 0
+# for i in range(len(a)):
+#     if a[i] == '.':
+#         pass
+#     else:
+#         sum += int(a[i])
+# print(sum)
+
+
+# n = int(input())
+# x = 1
+# for i in range(n):
+#     print(x)
+#     x*=i+2
+
+# n = int(input())
+# sum = 0
+# x = 1
+# while n >= x:
+#     sum += (1 + 1 / x) ** x
+#     x+=1
+# print(sum)
+
+
+# n = int(input())
+# nm = -n
+# array = []
+# a = 2
+# b = -2
+# while n >= nm:
+#     array.append(nm)
+#     nm += 1
+# print(array)
+# print(array[a] * array[b])
+
+
+# array = [1, 2, 3, 4, 5, 6]
+# for i in range(len(array)):
+#     x = random.randint(0, len(array)-1)
+#     array[i], array[x] = array[x], array[i]
+# print(array)
